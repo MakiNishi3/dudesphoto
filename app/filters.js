ec78bbcd94187bd3a406e403b0c2bc17071702ee
Maki Nishi <tantamoraru49@gmail.com>
2026-04-18 09:38:03 +0200

    Refactor filter imports and registry
    Refactor filter imports and registry structure.

--- app/filters.js
+++ app/filters.js
@@ -1,35 +1,37 @@
-import angular from "/filters/effects/angular.js";
-import labadjust from "/filters/effects/labadjust.js";
-import fisheye from "/filters/effects/fisheye.js";
-import polarwarp from "/filters/effects/polarwarp.js";
-import rotate from "/filters/effects/rotate.js";
-import swirl from "/filters/effects/swirl.js";
-import wave from "/filters/effects/wave.js";
-import zigzag from "/filters/effects/zigzag.js";
-import brightness from "/filters/fxslash/brigtness.js";
-import contrast from "/filters/fxslash/contrast.js";
-import curves from "/filters/fxslash/curves.js";
-import saturation from "/filters/fxslash/saturation.js";
-import grayscale from "/filters/fxslash/grayscale.js";
-import hue from "/filters/fxslash/hue.js";
-import invert from "/filters/fxslash/invert.js";
+import angular from "/filters/effects/angular.js"
+import labadjust from "/filters/effects/labadjust.js"
+import fisheye from "/filters/effects/fisheye.js"
+import polarwarp from "/filters/effects/polarwarp.js"
+import rotate from "/filters/effects/rotate.js"
+import swirl from "/filters/effects/swirl.js"
+import wave from "/filters/effects/wave.js"
+import zigzag from "/filters/effects/zigzag.js"
+import brightness from "/filters/fxslash/brigtness.js"
+import contrast from "/filters/fxslash/contrast.js"
+import curves from "/filters/fxslash/curves.js"
+import saturation from "/filters/fxslash/saturation.js"
+import grayscale from "/filters/fxslash/grayscale.js"
+import hue from "/filters/fxslash/hue.js"
+import invert from "/filters/fxslash/invert.js"
 
 const registry = {
-  angular,
-  labadjust,
-  fisheye,
-  polarwarp,
-  rotate,
-  swirl,
-  wave,
-  zigzag,
-  brightness,
-  contrast,
-  curves,
-  saturation,
-  grayscale,
-  hue,
-  invert
-};
+angular,
+labadjust,
+fisheye,
+polarwarp,
+rotate,
+swirl,
+wave,
+zigzag,
+brightness,
+contrast,
+curves,
+saturation,
+grayscale,
+hue,
+invert
+}
 
-export default registry;
+const effects = registry
+
+const optionValueEffects = Object.values(registry)
